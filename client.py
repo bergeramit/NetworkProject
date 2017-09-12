a2a08c42cd046cf00af2a5617103ef4bfbcea111
bergeramit <bergeramit@gmail.com>
2017-09-12 19:15:48 +0300

    Got a normal server running send and recieve

--- client.py
+++ client.py
@@ -1,10 +1,23 @@
-
-
-
+import socket
 
 def mainClient():
     '''client stuff'''
-    pass
+    # create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    # get local machine name
+    host = socket.gethostname()                           
+    port = 9000
+    # connection to hostname on the port.
+    s.connect((host, port))    
+    dataToSend = "hello server from client"
+    s.send(dataToSend)                           
+    # Receive no more than 1024 bytes
+    tm = s.recv(1024)  
+    print("data got from server: %s" % tm.decode('ascii'))                                   
+    s.close()
 
 if __name__ == "__main__":
     mainClient()
+
+
+
--- server.py
+++ server.py
@@ -1,8 +1,25 @@
-
+import socket                                         
+import time
 
 def mainServer():
     '''server stuff'''
-    pass
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    host = socket.gethostname() 
+    port = 9000
+    serversocket.bind((host, port))
+    #need to listen to up to 5 requests
+    serversocket.listen(5)  
+    print("Listening...")
+    while True:
+        # establish a connection
+        clientsocket, addr = serversocket.accept()      
+        print("Got a connection from %s" % str(addr))
+        data = clientsocket.recv(1024)
+        if not data: 
+            pass
+        else:
+            clientsocket.send(data.encode('ascii'))
+            clientsocket.close()
 
 if __name__ == "__main__":
     mainServer()
